b8e4232bc49f1f7a9cd79ae6c773b09569cd6bd4
priya <priyadhayapule99@gmail.com>
2018-08-24 11:34:18 +0530

    First commit

---
+++ js/app.js
@@ -0,0 +1,114 @@
+// variables
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      subject = document.getElementById('subject'),
+      message = document.getElementById('message'),
+      resetBtn = document.getElementById('resetBtn'),
+      sendEmailForm = document.getElementById('email-form');
+
+
+// Event Listeners
+eventListeners();
+
+function eventListeners() {
+      // App Init
+      document.addEventListener('DOMContentLoaded', appInit);
+
+      // validate the forms
+      email.addEventListener('blur', validateField);
+      subject.addEventListener('blur', validateField);
+      message.addEventListener('blur', validateField);
+
+      // send email & reset button
+      sendEmailForm.addEventListener('submit', sendEmail);
+      resetBtn.addEventListener('click', resetForm);
+}
+
+
+
+// Functions
+
+// App Initialization
+function appInit() {
+      // disable the send button on load
+      sendBtn.disabled = true;
+
+}
+function sendEmail(e) {
+      e.preventDefault();
+
+      // show the spinner
+      const spinner = document.querySelector('#spinner');
+      spinner.style.display = 'block';
+
+      // Hide spinner then show the send Email image
+      setTimeout(function(){
+            // Hide the spinner after 3 sec 
+            spinner.style.display = 'none';
+
+            // show image 
+            const mail = document.querySelector('#mail');
+            mail.style.display = 'block';
+     // after 5 seconds, hide the image and reset the form
+            setTimeout(function(){
+            mail.style.display = 'none';
+            sendEmailForm.reset()
+            }, 5000 );        
+      }, 3000 );
+
+} 
+
+// validate the fields
+function validateField() {
+    let errors;
+
+    // validate the length of the field
+    validateLength(this);
+
+    // validate the email
+    if(this.type === 'email') {
+          validateEmail(this);
+    }
+
+    // Both will return errors,then check if there're any errors
+    errors = document.querySelectorAll('.error'); 
+
+    // check that the inouts are not empty
+    if(email.value !== '' && subject.value !== '' && message.value !== '') {
+          if(errors.length === 0) {
+                // the button should be enabled
+                sendBtn.disabled = false;
+          }
+
+    }
+    
+}
+
+// validate the length of the fields
+function validateLength(field) {
+      if(field.value.length > 0){
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+      } else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+      }
+}
+
+// validate email (checks for @ in the value)
+function validateEmail(field) {
+      let emailText = field.value;
+      // check if the emailText contains the @ sign
+      if(emailText.indexOf('@') !== -1) {
+            field.style.borderBottomColor = 'green';
+                  field.classList.remove('error');
+            } else {
+                  field.style.borderBottomColor = 'red';
+                  field.classList.add('error');
+            }
+      } 
+      // Reset the form
+      function resetForm() {
+            sendEmailForm.reset();
+      }    
+      
